d10dd0c1948cff7bb12b46d3ab8b7334c130ac86
Ibrahimilyas7 <mohdibrahim062003@gmail.com>
2025-11-14 21:32:36 +0530

    Update git.py

--- git.py
+++ git.py
@@ -1,1 +1,1 @@
-print("hello guys this is the file by the name git.py we are using this file to convert it from untrack to track using git command ,thank you")
+print("hello guys this is the file by the name git.py we are using this file to convert it from untrack to track using git command lets conclude the talk,thank you")
